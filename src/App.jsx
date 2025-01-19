87c29ccb1ea8089d97dc6e1d5961d28a8bbeb02d
Mara Liwayway David <davidmaraliwayway@gmail.com>
2025-01-18 20:39:34 -0800

    LeetCode frontend edits

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Index from "./Index.jsx";
 
 import './css/App.css'
+import LeetCode from './LeetCode.jsx';
 
 function App() {
   const [count, setCount] = useState(0)
@@ -12,8 +13,8 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Index />}>
-        </Route>
+        <Route path="/" element={<Index />}></Route>
+        <Route path="leetcode" element={<LeetCode />}></Route>
       </Routes>
     </BrowserRouter>
 
